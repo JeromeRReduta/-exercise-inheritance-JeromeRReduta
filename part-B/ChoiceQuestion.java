4e8ca9cb5687c9598451761a46aea420d4add60f
Lisa Leese <mlleese@usfca.edu>
2019-03-26 13:34:13 -0700

    initial files

---
+++ part-B/ChoiceQuestion.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+/**
+   A question with multiple choices.
+*/
+public class ChoiceQuestion
+{
+   private ArrayList<String> choices;
+
+   /**
+      Constructs a choice question with no choices.
+   */
+   public ChoiceQuestion()
+   {
+
+   }
+
+   /**
+      Adds an answer choice to this question.
+      @param choice the choice to add
+      @param correct true if this is the correct choice, false otherwise
+   */
+   public void addChoice(String choice, boolean correct)
+   {
+      choices.add(choice);
+      if (correct) 
+      {
+         // Convert choices.size() to string
+      }
+   }
+   
+   public void display()
+   {
+      // Display the question text
+      // Display the answer choices
+   }
+}
+
