6cc1ddf39f064044c50ea82c18dfcc030d29b9d8
JeromeRReduta <38510370+JeromeRReduta@users.noreply.github.com>
2019-04-03 14:13:07 -0700

    Final vers. (also made answer protected, not private)

---
+++ part-A/SkyNet.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class SkyNet {
+
+   /* public static void main(String[] args) {
+        System.out.println("Yes");
+        TimeUnit.SECONDS.sleep(4);
+        System.out.println("No");
+    }*/
+}
